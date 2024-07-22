9d464feb2e4c1ab49af64487ec8d6f49635f2a90
Sujal Mahalaha <112002169+sujalmahalaha@users.noreply.github.com>
2024-07-22 15:42:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -10,36 +10,49 @@ using namespace std;
 class Solution{
 public:
 
-     int solve(int W, int *wt, int *val, int n , vector<vector<int>>&dp){
-        if(n == 0){
-            if(wt[0] <= W){
-                int n = W/wt[0];
-                return n*val[0];
+    //  int solve(int W, int *wt, int *val, int n , vector<vector<int>>&dp){
+    //     if(n == 0){
+    //         if(wt[0] <= W){
+    //             int n = W/wt[0];
+    //             return n*val[0];
+    //         }
+    //         else{
+    //             return 0;
+    //         }
+    //     }
+    //       if(dp[n][W]  != -1){
+    //           return dp[n][W];
+    //       }
+    //         int notTake = solve(W , wt , val , n-1, dp);
+    //         int Take = INT_MIN;
+    //         if(wt[n] <= W){
+    //             Take = val[n] + solve(W-wt[n] , wt , val , n, dp);
+    //         }
+    //         return dp[n][W]= max(notTake  , Take);
+        
+        
+   int knapSack(int N, int W, int val[], int wt[]) {
+    vector<int> dp(W + 1, 0);
+
+    // Initial value assignment for the first item
+    for (int i = wt[0]; i <= W; i++) {
+        dp[i] = (i / wt[0]) * val[0];
+    }
+
+    // Fill the dp array for the rest of the items
+    for (int i = 1; i < N; i++) {
+        for (int w = wt[i]; w <= W; w++) {
+            int notTake = dp[w];
+            int take = INT_MIN;
+            if (wt[i] <= w) {
+                take = val[i] + dp[w - wt[i]];
             }
-            else{
-                return 0;
-            }
+            dp[w] = max(notTake, take);
         }
-           if(dp[n][W]  != -1){
-               return dp[n][W];
-           }
-            int notTake = solve(W , wt , val , n-1, dp);
-            int Take = INT_MIN;
-            if(wt[n] <= W){
-                Take = val[n] + solve(W-wt[n] , wt , val , n, dp);
-            }
-            return dp[n][W]= max(notTake  , Take);
-        
-        
     }
-    int knapSack(int N, int W, int val[], int wt[])
-    {
-        // code here
-         // Your code here
-         int n = N;
-       vector<vector<int>>dp(n , vector<int>(W+1 , -1));
-       return solve(W , wt , val , n-1, dp);
-    }
+
+    return dp[W];
+}
 };
 
 //{ Driver Code Starts.
