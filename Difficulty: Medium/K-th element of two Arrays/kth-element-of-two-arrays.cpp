028cf4cc20e5d280af8aab0e09a342bf7ace5dc8
Sujal Mahalaha <112002169+sujalmahalaha@users.noreply.github.com>
2024-08-07 21:03:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
+        // code here
+          multiset<int> st;
+        for(int i=0;i<arr1.size();i++)
+        {
+            st.insert(arr1[i]);
+        }
+        for(int i=0;i<arr2.size();i++)
+        {
+            st.insert(arr2[i]);
+        }
+        set<int>::iterator it=st.begin();
+        advance(it,k-1);
+        return *it;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, m, k;
+        cin >> k;
+        cin.ignore();
+        string input;
+        int num;
+        vector<int> arr1, arr2;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr1.push_back(num);
+        }
+
+        getline(cin, input);
+        stringstream s3(input);
+        while (s3 >> num) {
+            arr2.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.kthElement(k, arr1, arr2) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
