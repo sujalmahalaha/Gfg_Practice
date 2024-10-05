fe243b47dbf46ec5b1704c93fa15c0a93e95ca50
Sujal Mahalaha <112002169+sujalmahalaha@users.noreply.github.com>
2024-10-05 22:57:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Not a subset sum/not-a-subset-sum.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long findSmallest(vector<int> &arr) {
+        long long sum = 0; sort(arr.begin(),arr.end());
+        for ( int i = 0; i<arr.size(); i++ ){
+            if ( sum+1 < arr[i] ) return sum+1;
+            sum += arr[i];
+        } return sum+1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        auto ans = ob.findSmallest(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
